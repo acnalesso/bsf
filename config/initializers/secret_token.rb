6f63ff7b764198751f35d1f43999a7a497753c91
Jason Hsu <jhsu@debian.debianserver.com>
2013-04-22 20:25:18 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bsf::Application.config.secret_token = '4ebfe800fbae8e84c96821429cc5f975b0d55c23b781ea83939a4c1bdf2ac67940f45ec86ba1bac1acc05d7a9a00f39bdfd955a4a97d9233753afc5539bd30eb'
